b7e80664d3f67c897fb8c1bef33707051a745e56
lxl66566 <lxl66566@gmail.com>
2026-06-06 10:56:17 +0800

    fix: vite build not show highlight
    Signed-off-by: lxl66566 <lxl66566@gmail.com>

--- frontend/src/prism.ts
+++ frontend/src/prism.ts
@@ -1,5 +1,21 @@
 import { languages } from "solid-prism-editor/prism";
 
+// JSON grammar (inline to prevent tree-shaking)
+languages.json ??= {
+  property: /"(?:\\.|[^\\\n"])*"(?=\s*:)/g,
+  string: /"(?:\\.|[^\\\n"])*"/g,
+  comment: /\/\/.*|\/\*[^]*?(?:\*\/|$)/g,
+  number: /-?\b\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
+  operator: /:/,
+  punctuation: /[[\]{},]/,
+  boolean: /\b(?:false|true)\b/,
+  null: {
+    pattern: /\bnull\b/,
+    alias: "keyword",
+  },
+};
+
+// dae grammar
 const daeId = /[a-zA-Z_][a-zA-Z0-9_\/\\^*.+\-@$!#%]*/;
 
 languages.dae = {
